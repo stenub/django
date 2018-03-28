09abf1d9b7047a828d004067b290e7aa95234912
root <root@sn-client.centralnetpoint.com>
2018-03-28 17:59:24 +0200

    commit 2

--- pegasus/mtbackup/urls.py
+++ pegasus/mtbackup/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('new_device', views.new_device),
+    path('show_device', views.show_device),
     path('get_backup', views.get_backup),
     
 ]
--- pegasus/mtbackup/views.py
+++ pegasus/mtbackup/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 
 from .forms import new_DeviceForm
 
+from .models import Device
+
 
 def index(request):
     return HttpResponse("Hello World. Du bist im mtbackup index.")
@@ -13,5 +15,17 @@ def get_backup(request):
     return HttpResponse("Das ist der get_backup view.")
 
 def new_device(request):
-    form = new_DeviceForm()
-    return render(request, 'mtbackup/new_device.html', {'form': form})
+    if request.method == 'GET':
+        form = new_DeviceForm()
+    elif request.method == 'POST':
+        form = new_DeviceForm(request.POST)
+        if form.is_valid():
+            data = form.save()
+            data.save()
+            return HttpResponse("Speichern erfolgreich!")
+    return render(request, 'mtbackup/new_device.html', {'form': form})        
+        
+
+def show_device(request):
+    devices = Device.objects.all()
+    return HttpResponse(devices)
